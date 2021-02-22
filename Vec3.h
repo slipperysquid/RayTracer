d23dd231e6428fd290be196f8aabbeca07f0513c
slipperysquid <coleseto123@gmail.com>
2021-02-22 13:43:16 -0800

    implementing ray tracing into rendering

--- Vec3.h
+++ Vec3.h
@@ -63,24 +63,28 @@ inline ostream& operator <<(ostream &out, const Vec3 &v) {
     return out << v.e[0] << " " << v.e[1] << " " << v.e[2];
 }
 
-inline Vec3 operator +(Vec3 &v, Vec3 &u){
+inline Vec3 operator +(const Vec3 &v, const Vec3 &u){
     return Vec3(v.e[0] + u.e[0], v.e[1] + u.e[1], v.e[2] + u.e[2]);
 }
 
-inline Vec3 operator -(Vec3 &v, Vec3 &u){
+inline Vec3 operator -(const Vec3 &v, const Vec3 &u){
     return Vec3(v.e[0] - u.e[0], v.e[1] - u.e[1], v.e[2] - u.e[2]);
 }
 
-inline Vec3 operator *(Vec3 &v, Vec3 &u){
+inline Vec3 operator *(const Vec3 &v, const Vec3 &u){
     return Vec3(v.e[0] * u.e[0], v.e[1] * u.e[1], v.e[2] * u.e[2]);
 }
 
-inline Vec3 operator *(double t, Vec3 &v){
+inline Vec3 operator *(double t, const Vec3 &v){
     return Vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
 inline Vec3 operator /=( Vec3 v, double t){
     return (1/t) * v;
+}
+
+inline double dot(const Vec3 &u, const Vec3 &v){
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
 
 inline Vec3 cross(const Vec3 &u, const Vec3 &v) {
--- main.cpp
+++ main.cpp
@@ -1,22 +1,48 @@
 #include "Colour.h"
 #include "Vec3.h"
+#include "Ray.h"
 
 #include <iostream>
 
 using namespace std;
 
+
+colour ray_colour(const Ray& r){
+    Vec3 unit_direction = unit_vector(r.get_direction());
+    auto t = 0.5 * (unit_direction.get_y() + 1.0);
+    return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
+}
+
+
 int main(){
     //Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int img_width = 400;
+    const int img_height = static_cast<int>(img_width / aspect_ratio);
 
-    const int img_height = 256;
-    const int img_width = 256;
+    //Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    //setting up view from camera
+    auto horizontal = Vec3(viewport_width, 0, 0);
+    auto vertical = Vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - vertical / 2 - horizontal / 2 - Vec3(0, 0, focal_length); 
 
     //Render --- will write into a plain text PPM format
     cout << "P3\n" << img_width << " " << img_height << "\n255\n";
     for (int j = img_height - 1; j >= 0; j--){
         cerr << "\n Scanlines remaining: " << j << ' ' << flush;
         for(int i = 0; i < img_width; i++){
-            colour pixel_colour(double(i) / (img_width - 1), double(j) / (img_height - 1), 0.25);//making a coulour vector containing the colour multiplyers
+            auto u = double(i) / (img_width - 1);
+            auto v = double(j) / (img_height - 1);
+
+            Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin); //subtract origin incase it is not (0, 0, 0)
+
+            colour pixel_colour = ray_colour(r);//making a coulour vector containing the colour multiplyers
             write_colour(cout, pixel_colour);//printing colour value of pixel
         }
     }
